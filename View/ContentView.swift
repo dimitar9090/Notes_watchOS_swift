2b3401ade92dae0bc5a3df89de1bdbc11c8199a4
Dimitar Anguelov <113285865+dimitar9090@users.noreply.github.com>
2024-01-13 23:08:46 +0200

    Update ContentView.swift

--- View/ContentView.swift
+++ View/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @AppStorage("lineCount") var lineCount: Int = 1
+    
     @State private var notes: [Note] = [Note]()
     @State private var text: String = ""
     
@@ -81,7 +83,7 @@ struct ContentView: View {
                                         .frame(width: 4)
                                         .foregroundColor(.accentColor)
                                     Text(notes[i].text)
-                                        .lineLimit(1)
+                                        .lineLimit(lineCount)
                                         .padding(.leading, 5)
                                 }
                             }//:HStack
